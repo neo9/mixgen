526164f0e0da92b83b40148abd9929207e5076d9
Benjamin Canac <canacb1@gmail.com>
2015-06-29 14:57:59 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var rimraf = require('gulp-rimraf');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var sass = require('gulp-sass');
+
+require('gulp-release-tasks')(gulp);
+
+var dist = 'dist';
+var assets = 'assets';
+
+var paths = {
+	less: [
+		assets + '/less/commons.less'
+	],
+	sass: [
+		assets + '/sass/commons.scss'
+	]
+};
+
+function handleError(err) {
+	console.warn(err.message);
+	this.emit('end');
+}
+
+gulp.task('less', [], function () {
+	gulp.src(paths.less)
+		.pipe(less().on('error', handleError))
+		.pipe(gulp.dest(dist + '/less'));
+});
+
+gulp.task('sass', [], function () {
+	gulp.src(paths.sass)
+		.pipe(sass().on('error', handleError))
+		.pipe(gulp.dest(dist + '/sass'));
+});
+
+gulp.task('clean', [], function () {
+	return gulp.src(dist, { read: false }).pipe(rimraf({ force: true }));
+});
+
+gulp.task('watch', [], function () {
+	[
+		gulp.watch(paths.less, [ 'less' ]),
+		gulp.watch(paths.scss, [ 'sass' ])
+	].forEach(function (watch) {
+			watch.on('change', function (event) {
+				console.log('File %s was %s, running tasks...', event.path, event.type);
+			});
+		});
+});
+
+gulp.task('default', [
+	'less',
+	'sass'
+]);
